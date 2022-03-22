0cfc5cdbe3088cbe7c69338ff664eb498c5605a8
Till Hermsen <t.hermsen@slashwhy.de>
2022-03-22 15:09:14 +0100

    Add navbar component, add redirect rule to redirect from / to /contracts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,14 @@ import '../../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { QUERY_CONFIG } from '../constant/query.config';
+import { Navbar } from '../modules/navbar';
 
 const queryClient = new QueryClient(QUERY_CONFIG);
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
         <QueryClientProvider client={queryClient}>
+            <Navbar />
             <Component {...pageProps} />
         </QueryClientProvider>
     );
---
+++ src/pages/usage.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from 'next';
+
+const Usage: NextPage = () => {
+    return <div>Usage..</div>;
+};
+
+export default Usage;
